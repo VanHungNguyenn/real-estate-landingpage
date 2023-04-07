206cbc0b329d92e7232514903c646cba44b7992d
VanHungNguyen <33939159+VanHungNguyenn@users.noreply.github.com>
2023-04-07 17:58:53 +0700

    header and how it work section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
 	content: ['./src/**/*.{html,js}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			dm: ['DM Sans', 'sans-serif'],
+			roboto: ['Roboto', 'sans-serif'],
+		},
+		colors: {
+			orangeff: '#ff9900',
+			orangef8: '#f85a47',
+			gray31: '#31353b',
+			grayda: '#dadada',
+			gray33: '#333333',
+		},
 	},
 	plugins: [],
 }
